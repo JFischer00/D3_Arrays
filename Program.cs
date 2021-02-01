5002dc12228b076bdb24cb93ce0cb76e8c89bf0c
Jordan Fischer <jordan@thefischers.org>
2021-02-01 18:18:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace D3_Arrays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = new int[25];
+
+			for (int i = 0; i < numbers.Length; i++)
+				numbers[i] = i + 1;
+
+			foreach (int i in numbers)
+				Console.WriteLine($"Element value = {i}");
+		}
+	}
+}
